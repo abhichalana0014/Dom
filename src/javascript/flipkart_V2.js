3de6e0938ddefb4c580d5a77fb78a62e329b3f4b
Abhishek.Chalana <abhishek.chalana@jptokyo.co.jp>
2023-10-27 20:44:06 +0530

    complete the dark mode task add toggle button  to change the header mode to dark. all this make in .js file.

--- src/javascript/flipkart_V2.js
+++ src/javascript/flipkart_V2.js
@@ -267,6 +267,145 @@ const products = [
   },
 ];
 
+// create header
+const header = document.createElement("header");
+header.classList.add("w-full", "py-3", "bg-blue-600", "dark:bg-black");
+
+// create navbar
+const nav = document.createElement("nav");
+nav.classList.add("flex", "mx-auto", "max-w-6xl");
+
+//create a div navbar
+let navContainer1 = document.createElement("div");
+navContainer1.classList.add("flex", "item-center", "gap-8", "w-full");
+
+//create navlogo
+let link = document.createElement("a");
+link.classList.add("w-36");
+link.href = "#";
+
+let navLogo = document.createElement("img");
+navLogo.classList.add("w-28");
+navLogo.src =
+  "https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/fk-plus_3b0baa.png";
+navLogo.alt = "Flipkart";
+link.append(navLogo);
+
+//create a form
+let form = document.createElement("form");
+form.classList.add("w-full");
+form.action = "search";
+
+// create a div in form
+let formContainer = document.createElement("div");
+formContainer.classList.add("relative");
+
+let inputLabel = document.createElement("label");
+inputLabel.for = "searchItems";
+inputLabel.classList.add("hidden");
+inputLabel.textContent = "Search Items";
+formContainer.appendChild(inputLabel);
+
+//create span in div
+let span = document.createElement("span");
+span.setAttribute("class", "absolute inset-y-0 right-0 flex items-center pr-2");
+
+//create button in span
+let spanbtn = document.createElement("button");
+spanbtn.type = "submit";
+spanbtn.classList.add("p-1", "focus:outline-none", "focus:shadow-outline");
+spanbtn.innerHTML = `<svg class="w-6 h-6 text-gray-500" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" ><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>`;
+span.appendChild(spanbtn);
+formContainer.appendChild(span);
+
+//create input in div
+
+let inputAttributes = {
+  tag: "input",
+  attributes: {
+    type: "text",
+    id: "js_search_input",
+    placeholder: "Search products",
+  },
+};
+let input = document.createElement(inputAttributes.tag);
+Object.assign(input, inputAttributes.attributes);
+
+input.setAttribute(
+  "class",
+  "w-full py-2 text-sm text-white bg-white rounded pl-2 focus:outline-none focus:bg-white focus:text-gray-900"
+);
+
+//create ul for search element list
+let ul = document.createElement("ul");
+ul.setAttribute(
+  "class",
+  "search-results absolute left-0 mt-8 w-full bg-white shadow-lg rounded-b-md hidden"
+);
+formContainer.appendChild(ul);
+formContainer.appendChild(input);
+form.appendChild(formContainer);
+navContainer1.appendChild(link);
+navContainer1.appendChild(form);
+
+// create a navcontainer2
+
+let navcontainer2 = document.createElement("div");
+navcontainer2.setAttribute(
+  "class",
+  "flex gap-4 items-center w-full text-center px-4"
+);
+
+let a1 = document.createElement("a");
+let a2 = document.createElement("a");
+let a3 = document.createElement("a");
+let span1 = document.createElement("span");
+let span2 = document.createElement("span");
+let span3 = document.createElement("span");
+
+a1.setAttribute("class", "flex items-center text-base font-mono text-white gap-2 pl-8 pr-4")
+a2.setAttribute("class", "flex items-center text-base font-mono text-white gap-2 px-4")
+a3.setAttribute("class", "flex items-center text-base font-mono text-white gap-2 px-4")
+
+a1.innerHTML = `<svg class="w-5 h-5 text-white" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+<path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0zM1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5zM4 15h3v-5H4v5zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3zm3 0h-2v3h2v-3z"></path>
+</svg>`;
+a2.innerHTML = `<svg class="w-5 h-5 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" fill="currentColor"><path d="M230.92,212c-15.23-26.33-38.7-45.21-66.09-54.16a72,72,0,1,0-73.66,0C63.78,166.78,40.31,185.66,25.08,212a8,8,0,1,0,13.85,8c18.84-32.56,52.14-52,89.07-52s70.23,19.44,89.07,52a8,8,0,1,0,13.85-8ZM72,96a56,56,0,1,1,56,56A56.06,56.06,0,0,1,72,96Z"></path></svg>`
+a3.innerHTML = `<svg class="w-5 h-5 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M6 6 L30 6 27 19 9 19 M27 23 L10 23 5 2 2 2"></path><circle cx="25" cy="27" r="2"></circle><circle cx="12" cy="27" r="2"></circle></svg>`
+
+span1.textContent = "Become a Seller"
+span2.textContent = "Login"
+span3.textContent = "Cart"
+
+a1.appendChild(span1);
+a2.appendChild(span2);
+a3.appendChild(span3);
+
+navcontainer2.appendChild(a1);
+navcontainer2.appendChild(a2);
+navcontainer2.appendChild(a3);
+
+
+let togglebtn = document.createElement("button");
+togglebtn.classList.add("px-2",
+"py-1",
+"text-white",
+"bg-gray-800",
+"rounded",
+"border",
+"border-gray-900",
+"dark:bg-white",
+"dark:text-black");
+togglebtn.textContent = "Darkmode";
+
+navcontainer2.appendChild(togglebtn);
+
+nav.appendChild(navContainer1);
+nav.appendChild(navcontainer2);
+header.appendChild(nav);
+
+document.body.appendChild(header);
+
 const enteredValue = (ev) => {
   let data = "";
   let ul = document.querySelector(".search-results");
@@ -307,6 +446,7 @@ const enteredValue = (ev) => {
 };
 const handleClickOutside = () => {
   ul.classList.add("hidden");
+  document.getElementById("js_search_input").value = ""
 };
 
 document
@@ -314,3 +454,14 @@ document
   .addEventListener("input", enteredValue);
 
 document.addEventListener("click", handleClickOutside);
+
+let mode = 1;
+const darkMode = () => {
+  let html = document.getElementById("js_dark");
+
+  togglebtn.innerHTML = mode ? "Lightmode" : "Darkmode";
+  html.setAttribute("class", mode ? "dark" : "");
+  mode = mode ? 0 : 1;
+};
+
+togglebtn.addEventListener("click", darkMode);
