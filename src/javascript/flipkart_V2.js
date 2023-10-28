befe01f9c336d1b02cb12a9f093079a49c0eb8bd
Abhishek.Chalana <abhishek.chalana@jptokyo.co.jp>
2023-10-28 10:54:01 +0530

    update the className in input attribute and add the href to anchor tag in  navcontainer2

--- src/javascript/flipkart_V2.js
+++ src/javascript/flipkart_V2.js
@@ -326,15 +326,11 @@ let inputAttributes = {
     type: "text",
     id: "js_search_input",
     placeholder: "Search products",
+    className : "w-full py-2 text-sm text-white bg-white rounded pl-2 focus:outline-none focus:bg-white focus:text-gray-900"
   },
 };
 let input = document.createElement(inputAttributes.tag);
 Object.assign(input, inputAttributes.attributes);
-
-input.setAttribute(
-  "class",
-  "w-full py-2 text-sm text-white bg-white rounded pl-2 focus:outline-none focus:bg-white focus:text-gray-900"
-);
 
 //create ul for search element list
 let ul = document.createElement("ul");
@@ -366,6 +362,10 @@ let span3 = document.createElement("span");
 a1.setAttribute("class", "flex items-center text-base font-mono text-white gap-2 pl-8 pr-4")
 a2.setAttribute("class", "flex items-center text-base font-mono text-white gap-2 px-4")
 a3.setAttribute("class", "flex items-center text-base font-mono text-white gap-2 px-4")
+
+a1.href = "#"
+a2.href = "#"
+a3.href = "#"
 
 a1.innerHTML = `<svg class="w-5 h-5 text-white" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
 <path d="M2.97 1.35A1 1 0 0 1 3.73 1h8.54a1 1 0 0 1 .76.35l2.609 3.044A1.5 1.5 0 0 1 16 5.37v.255a2.375 2.375 0 0 1-4.25 1.458A2.371 2.371 0 0 1 9.875 8 2.37 2.37 0 0 1 8 7.083 2.37 2.37 0 0 1 6.125 8a2.37 2.37 0 0 1-1.875-.917A2.375 2.375 0 0 1 0 5.625V5.37a1.5 1.5 0 0 1 .361-.976l2.61-3.045zm1.78 4.275a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0 1.375 1.375 0 1 0 2.75 0V5.37a.5.5 0 0 0-.12-.325L12.27 2H3.73L1.12 5.045A.5.5 0 0 0 1 5.37v.255a1.375 1.375 0 0 0 2.75 0 .5.5 0 0 1 1 0zM1.5 8.5A.5.5 0 0 1 2 9v6h1v-5a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1v5h6V9a.5.5 0 0 1 1 0v6h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1V9a.5.5 0 0 1 .5-.5zM4 15h3v-5H4v5zm5-5a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1v-3zm3 0h-2v3h2v-3z"></path>
