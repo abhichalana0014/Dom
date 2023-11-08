bf846dbb533eca205923075b8e6c34038788703e
Abhishek.Chalana <abhishek.chalana@jptokyo.co.jp>
2023-11-09 01:43:10 +0530

    Multiple alerts , slider, contextMenu  done,   scroll to top , and upside down slider pendig

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation:{
+        "bottomslider": "bottomslider 6s linear forwards",
+        "moveleft": "moveleft 1s linear forwards",
+      },
+      keyframes: {
+        bottomslider:{
+          "100%":{
+            width:0
+          }
+        },
+        moveleft:{
+          "100%":{
+            transform: "translateX(0)"
+          }
+        }
+      }
+    },
   },
   plugins: [],
   darkMode: 'class',
